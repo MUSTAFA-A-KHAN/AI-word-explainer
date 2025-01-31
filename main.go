df37f929ed48330e3542798da4170e675ee1c8c5
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-01-31 15:31:15 +0530

    feat: multi layer ed neuron like in millions thrn use loops

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func main() {
 
 	inputs := []float32{1, 2, 3, 2.5}
 
+	weights := [][]float32{
+		{0.2, 0.8, -0.5, 1.0},
+		{0.5, -0.91, 0.26, -0.5},
+		{-0.26, -0.27, 0.17, 0.87},
+	}
+
 	weights1 := []float32{0.2, 0.8, -0.5, 1.0}
 	weights2 := []float32{0.5, -0.91, 0.26, -0.5}
 	weights3 := []float32{-0.26, -0.27, 0.17, 0.87}
@@ -34,4 +40,20 @@ func main() {
 		bias3)
 	fmt.Println(outputs)
 
+	biases := []float32{2.0, 3.0, 0.5}
+
+	layerOutputs := make([]float32, 3)
+
+	for i := range weights {
+		neuronWeight := weights[i]
+		neuronBias := biases[i]
+		var neuronOutput float32
+		for j := range inputs {
+			neuronOutput += inputs[j] * neuronWeight[j]
+		}
+		neuronOutput += neuronBias
+		layerOutputs[i] = neuronOutput
+
+	}
+	fmt.Println(layerOutputs)
 }
