75d4f09ae75c4c5fea81391d710e00bcd8077062
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-01-31 15:48:38 +0530

    feat: neuron using for loop.

--- main.go
+++ main.go
@@ -12,34 +12,6 @@ func main() {
 		{-0.26, -0.27, 0.17, 0.87},
 	}
 
-	weights1 := []float32{0.2, 0.8, -0.5, 1.0}
-	weights2 := []float32{0.5, -0.91, 0.26, -0.5}
-	weights3 := []float32{-0.26, -0.27, 0.17, 0.87}
-
-	var bias1 float32 = 2.0
-	var bias2 float32 = 3.0
-	var bias3 float32 = 0.5
-
-	outputs := make([]float32, 3)
-
-	outputs[0] = (inputs[0]*weights1[0] +
-		inputs[1]*weights1[1] +
-		inputs[2]*weights1[2] +
-		inputs[3]*weights1[3] + bias1)
-
-	outputs[1] = (inputs[0]*weights2[0] +
-		inputs[1]*weights2[1] +
-		inputs[2]*weights2[2] +
-		inputs[3]*weights2[3] +
-		bias2)
-
-	outputs[2] = (inputs[0]*weights3[0] +
-		inputs[1]*weights3[1] +
-		inputs[2]*weights3[2] +
-		inputs[3]*weights3[3] +
-		bias3)
-	fmt.Println(outputs)
-
 	biases := []float32{2.0, 3.0, 0.5}
 
 	layerOutputs := make([]float32, 3)
