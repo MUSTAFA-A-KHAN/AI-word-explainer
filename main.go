ce2b6b8c3547c3f1b771febb47a4ce274b26e765
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-02-02 15:17:01 +0530

    feat: using raw weights for dotproduct to generate output.

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 )
 
 func main() {
+
+	rawWeights := []float32{
+		0.2, 0.8, -0.5, 1.0,
+		0.5, -0.91, 0.26, -0.5,
+		-0.26, -0.27, 0.17, 0.87,
+	}
+
 	inputs := t.New(t.WithBacking([]float32{1, 2, 3, 2.5}))
-	weights := t.New(t.WithBacking([]float32{0.2, 0.8, -0.5, 1.0}))
+	weights := t.New(t.WithShape(3, 4), t.WithBacking(rawWeights))
 	biase := t.New(t.WithBacking([]float32{2.0}))
 
 	dotproduct, _ := t.Dot(weights, inputs)
