013de311b2283a119638dae8afc3018789899c47
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-02-05 11:30:33 +0530

    feat: separate out dataset into training and test data set with the rario 1:5

---
+++ chapter1/evalution-and-validation/validation/validate.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/go-gota/gota/dataframe"
+)
+
+func main() {
+	f, err := os.Open("./data/diabetes.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	diabetesDF := dataframe.ReadCSV(f)
+
+	trainingNum := (4 * diabetesDF.Nrow()) / 5
+	testNum := diabetesDF.Nrow() / 5
+
+	if trainingNum+testNum < diabetesDF.Nrow() {
+		trainingNum++
+	}
+	trainingIdx := make([]int, trainingNum)
+	testIdx := make([]int, testNum)
+	// Populate the trainingidx
+	for i := 0; i < trainingNum; i++ {
+		trainingIdx[i] = i
+	}
+	for i := 0; i < testNum; i++ {
+		testIdx[i] = trainingNum + i
+	}
+	trainingDF := diabetesDF.Subset(trainingIdx)
+	testDF := diabetesDF.Subset(testIdx)
+
+	setMap := map[int]dataframe.DataFrame{
+		0: trainingDF,
+		1: testDF,
+	}
+	for idx, setName := range []string{"./data/training.csv", "./data/test.csv"} {
+		f, err := os.Create(setName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w := bufio.NewWriter(f)
+		if err := setMap[idx].WriteCSV(w); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
